617426dc638e7d25bf6066838f6d20b9592fc9a6
kankittikan <nongkan195@gmail.com>
2023-08-20 22:23:11 +0700

    add video in space

--- pages/space.vue
+++ pages/space.vue
@@ -2,7 +2,12 @@
     <Head>
         <Title>Space - Doksakura</Title>
     </Head>
-    <div class="bg-black h-screen">
-        <h1>Space Site</h1>
+    <div class="bg-black h-screen bg-[url('/images/earth.jpg')] bg-fixed bg-cover">
+        <div class="p-10 flex justify-center">
+            <h1 class="text-6xl bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-emerald-500">Saturday Team</h1>
+        </div>
+        <div class="p-2 flex justify-center">
+            <video src="https://content.doksakura.com/videos/15F04F39-18D6-4A46-B1AB-6AA7EE591ECE.mp4" class="w-[800px]" autoplay/>
+        </div>
     </div>
 </template>
