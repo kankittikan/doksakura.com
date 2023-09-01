6ccdd7b63b509292cdf3b60c2b555c8c6b63df01
kankittikan <nongkan195@gmail.com>
2023-09-01 20:53:58 +0700

    change nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,8 +4,7 @@
       <img src="/favicon.ico" />
     </div>
     <NuxtLink to="./" class="text-white px-2 ml-4 hover:bg-pink-400 py-5">Home</NuxtLink>
-    <NuxtLink to="minecraft" class="text-white px-2 hover:bg-pink-400 py-5">Minecraft
-      Server</NuxtLink>
+    <NuxtLink to="minecraft" class="text-white px-2 hover:bg-pink-400 py-5">Minecraft</NuxtLink>
     <NuxtLink to="space" class="text-white px-2 hover:bg-pink-400 py-5">Space</NuxtLink>
     <NuxtLink to="https://access.doksakura.com" target="_blank" class="text-white px-2 hover:bg-pink-400 py-5">Access</NuxtLink>
     <NuxtLink to="http://vault.doksakura.com:30817" target="_blank" class="text-white px-2 hover:bg-pink-400 py-5">Vault</NuxtLink>
