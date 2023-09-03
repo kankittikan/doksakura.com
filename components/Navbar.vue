fdc45617bb8bd3ccba0538f897e459b9d394b2e2
kankittikan <nongkan195@gmail.com>
2023-09-03 15:34:04 +0700

    remove valve

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,6 +7,5 @@
     <NuxtLink to="minecraft" class="text-white px-2 hover:bg-pink-400 py-5">Minecraft</NuxtLink>
     <NuxtLink to="space" class="text-white px-2 hover:bg-pink-400 py-5">Space</NuxtLink>
     <NuxtLink to="https://access.doksakura.com" target="_blank" class="text-white px-2 hover:bg-pink-400 py-5">Access</NuxtLink>
-    <NuxtLink to="http://vault.doksakura.com:30817" target="_blank" class="text-white px-2 hover:bg-pink-400 py-5">Vault</NuxtLink>
   </header>
 </template>
