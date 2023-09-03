578528386c02b97568334a8a3acddf694d80dc81
kankittikan <nongkan195@gmail.com>
2023-09-04 00:59:52 +0700

    add click sound

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,11 +1,19 @@
 <template>
+  <audio src="/sound/click1.wav" id="s"/>
   <header class="bg-gradient-to-r from-pink-500 to-purple-500 flex h-16">
     <div class="w-10 my-auto ml-3">
       <img src="/favicon.ico" />
     </div>
-    <NuxtLink to="./" class="text-white px-2 ml-4 hover:bg-pink-400 py-5">Home</NuxtLink>
-    <NuxtLink to="minecraft" class="text-white px-2 hover:bg-pink-400 py-5">Minecraft</NuxtLink>
-    <NuxtLink to="space" class="text-white px-2 hover:bg-pink-400 py-5">Space</NuxtLink>
-    <NuxtLink to="https://access.doksakura.com" target="_blank" class="text-white px-2 hover:bg-pink-400 py-5">Access</NuxtLink>
+    <NuxtLink to="./" class="text-white px-2 ml-4 hover:bg-pink-400 py-5" @click="playSound">Home</NuxtLink>
+    <NuxtLink to="minecraft" class="text-white px-2 hover:bg-pink-400 py-5" @click="playSound">Minecraft</NuxtLink>
+    <NuxtLink to="space" class="text-white px-2 hover:bg-pink-400 py-5" @click="playSound">Space</NuxtLink>
+    <NuxtLink to="https://access.doksakura.com" target="_blank" class="text-white px-2 hover:bg-pink-400 py-5" @click="playSound">Access</NuxtLink>
   </header>
-</template>
+</template>  
+
+<script lang="ts" setup>
+  const playSound = () => {
+    var audio1 = document.getElementById("s");
+    audio1.play();
+  }
+</script>
